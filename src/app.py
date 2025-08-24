e92d5aee8c91b429b9e2dc70f33da7c5331191e3
Eduardo Rangel <eduaran@hotmail.com>
2025-08-23 23:14:17 -0600

    Adds python cicd fix1

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def info():
     return jsonify({
             'time':datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
             'hostname': socket.gethostname(),
-            'message': 'You are doing grat, human!!! :)',
+            'message': 'You are doing grat, human! :)',
             'deployed_on': 'kubernetes'
     }) 
 
